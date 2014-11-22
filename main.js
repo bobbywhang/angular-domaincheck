6d7f8bde59dec86135322073441e3c72e601ab63
bobbywhang <robertwhang@gmail.com>
2014-11-22 15:22:46 -0800

    added jsonp endpoint

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', function($scope, $http){
-	$scope.message = "Yoman";
-	
+
 	// var userDomainCheck = $scope.userDomainCheck;
 
 	$scope.getURL = function(userDomainCheck) {
 	//return the promise directly.
-	return $http.get('http://domains.bootname.com/api/v1/domain/' + userDomainCheck)
+	return $http.jsonp('http://domains.bootname.com/api/v1/domain/' + userDomainCheck + '?callback=JSON_CALLBACK')
 		.then(function(result) {
 	//resolve the promise as the data
-	console.log(result);
+	console.log(result.data);
 	});
 
 };
