65a3a375a655f723dfacc4a7d8eb13cced2116a2
bobbywhang <robertwhang@gmail.com>
2014-11-22 17:23:46 -0800

    added the add-to-list functionality

--- main.js
+++ main.js
@@ -4,14 +4,27 @@ myApp.controller('mainController', function($scope, $http){
 
 	// var userDomainCheck = $scope.userDomainCheck;
 
+
+
 	$scope.getURL = function(userDomainCheck) {
-	//return the promise directly.
-	return $http.jsonp('http://domains.bootname.com/api/v1/domain/' + userDomainCheck + '?callback=JSON_CALLBACK')
-		.then(function(result) {
-	//resolve the promise as the data
-	console.log(result.data);
+		//return the promise directly.
+		$scope.domainResults = [];
+		return $http.jsonp('http://domains.bootname.com/api/v1/domain/' + userDomainCheck + '?callback=JSON_CALLBACK')
+			// .then means "do this then do this"
+			.then(function(result) {
+				console.log(result.data);
+				for(var i = 0; i < result.data.length; i++) {
+					$scope.domainResults.push(result.data[i]);
+				}
+			});
+
+		};
+
+		$scope.itemArray = [];
+		$scope.addToList = function(item) {
+			$scope.itemArray.push(item);	
+		}
+
+
+
 	});
-
-};
-
-});
